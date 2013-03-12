c7ec220794b860b419ea72a1e70ca755d4daacf1
Pablo Viquez <pviquez@pabloviquez.com>
2013-03-12 11:48:55 -0600

    Added JSDocs to the donations.js file. Did a small refactor to the donations.js file

--- src/js/donations.js
+++ src/js/donations.js
@@ -1,43 +1,47 @@
-/**
- * Donations page JS functionality
- *
- * @author Pablo Viquez <pviquez@pabloviquez.com>
- */
 Wiki = window.Wiki || {};
 
-Wiki.Donations = {
+/**
+ * @description Donations page, JS functionality
+ * @author Pablo Viquez <pviquez@pabloviquez.com>
+ * @version 1.1
+ */
+Wiki.Donations = (function() {
     /**
      * Original dollar amounts
      */
-    dollarAmounts : [],
+    var _dollarAmounts = [];
 
     /**
-     * Initialize the object, called when doc is ready
+     * @private
+     * @function _init
+     * @description Initialize the object, called when doc is ready
+     * @return void
      */
-    init : function() {
-        //self.loadCurrencies;
-        Wiki.Donations.loadCurrencies();
+    var _init = function() {
+        // Load the currencies from the AJAX endpoint
+        loadCurrencies();
 
         // Lets now store the curencies
         $("#donations > button").each(function (i) {
-            Wiki.Donations.dollarAmounts[i] = $(this).text();
+            _dollarAmounts[i] = $(this).text();
         });
 
         // Set the event handlers
-        $("#currencySelector").change(function() { Wiki.Donations.updateDonationButtons(); });
+        $("#currencySelector").change(function() { updateDonationButtons(); });
 
         // Set the handlers for the buttons
         $("#donations > button").click(function() {
             amount = $(this).text();
             alert("Thank you for your " + amount +  " donation!");
         });
-    },
+    };
 
     /**
-     * Queries the AJAX service and retrieves the data
+     * @description Queries the AJAX service and retrieves the data
+     * @private
      * @return void
      */
-    loadCurrencies : function () {
+    var loadCurrencies = function () {
         $.get('ajax/currencies.php', function (data) {
             dropdown = $("#currencySelector");
 
@@ -45,33 +49,43 @@ Wiki.Donations = {
                 dropdown.append($("<option />").val(data[item]).text(data[item]));
             });
         }, "json");
-    },
+    };
 
     /**
-     * Queries the conversion web service and updates the buttons with the
-     * correct amounts to display
+     * @description Queries the conversion web service and updates the buttons
+     *              with the correct amounts to display
+     * @private
      * @return void
      */
-    updateDonationButtons : function() {
+    var updateDonationButtons = function() {
         // Get the currency selected
         var currency = $("#currencySelector").val();
 
         if (currency === 'USD') {
             $("#donations > button").each(function (i) {
-                $(this).text(Wiki.Donations.dollarAmounts[i]);
+                $(this).text(_dollarAmounts[i]);
             });
 
             return;
         }
 
         // Make the call
-        $.get('ajax/conversions.php', { "currency" : currency, "data" : Wiki.Donations.dollarAmounts.join(":") }, function (data) {
+        $.get('ajax/conversions.php', { "currency" : currency, "data" : _dollarAmounts.join(":") }, function (data) {
             $("#donations > button").each(function (i) {
                 $(this).text(data[i]);
             });
         });
-    }
-};
+    };
 
-$(Wiki.Donations.init);
+    return {
+        /**
+         * @description Public alias for _init. Initializes handlers and vars
+         * @public
+         * @return void
+         */
+        init : _init
+    };
+})();
+
+$(Wiki.Donations.init());
 
