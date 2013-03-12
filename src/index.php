685d9323e8682f30e35f89bafecff8fb214cd4a6
Pablo Viquez <pviquez@pabloviquez.com>
2013-03-11 21:12:52 -0600

    Added minified version of the CSS file

--- src/index.php
+++ src/index.php
@@ -24,7 +24,7 @@ require_once '../lib/Config.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
-    <link href="css/global.css" rel="stylesheet" media="screen">
+    <link href="css/global.min.css" rel="stylesheet" media="screen">
 </head>
 <body>
     <div class="container">
